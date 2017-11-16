c1c763a79c9de27a1341a9f224f0b6be410726e6
Marina Gorlova <gorlovamarina@gmail.com>
2017-11-17 00:43:02 +0300

    VWU-15: Implemented WarmUp class

---
+++ app/src/main/java/com/berniac/vocalwarmup/Direction.java
@@ -0,0 +1,12 @@
+package com.berniac.vocalwarmup;
+
+/**
+ * Created by Marina on 14.11.2017.
+ */
+
+enum Direction {
+    START_TO_LOWER,
+    START_TO_UPPER,
+    LOWER_TO_START,
+    UPPER_TO_START
+}
---
+++ app/src/main/java/com/berniac/vocalwarmup/Step.java
@@ -0,0 +1,11 @@
+package com.berniac.vocalwarmup;
+
+/**
+ * Created by Marina on 14.11.2017.
+ */
+
+enum Step {
+    TONE,
+    HALF_TONE,
+    RANDOM
+}
